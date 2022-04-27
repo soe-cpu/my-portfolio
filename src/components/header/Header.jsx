ecd9868f4b229cff416e529ac0676c60c1b41ad3
soe@cpu <soelinaung709@gmail.com>
2022-04-27 12:45:39 +0630

    porfolio1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import './Header.css'
 import CTA from './CTA'
 import ME from '../../assets/pp1.png'
 import HeaderSocials from './HeaderSocials'
+import TypeAnimation from 'react-type-animation';
 
 const Header = () => {
   return (
@@ -10,6 +11,14 @@ const Header = () => {
       <div className="container header__container">
         <h4>Hello I'm</h4>
         <h1>Soe Lin Aung</h1>
+        <h3> 
+        <TypeAnimation
+          className="typeanimation"
+          repeat={Infinity}
+          sequence={['Web Developer', 1000, 'Web Designer', 1000, 'Fullstack Developer']}
+          wrapper="h3"
+        />
+        </h3>
         <h5 className="text-light">Web Developer</h5>
         <CTA />
         <HeaderSocials />
