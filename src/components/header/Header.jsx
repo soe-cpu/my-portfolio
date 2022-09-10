4ac8c155523dc5ae07d76baa86d4ed6d9f1ec3da
soe-cpu <soelinaung709@gmail.com>
2022-09-10 11:51:17 +0630

    some ui fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import './Header.css'
-import CTA from './CTA'
-import ME from '../../assets/pp1.png'
-import HeaderSocials from './HeaderSocials'
-import TypeAnimation from 'react-type-animation';
+import React from "react";
+import "./Header.css";
+import CTA from "./CTA";
+import ME from "../../assets/pp1.png";
+import HeaderSocials from "./HeaderSocials";
+import TypeAnimation from "react-type-animation";
 
 const Header = () => {
   return (
@@ -11,25 +11,42 @@ const Header = () => {
       <div className="container header__container">
         <h4>Hello I'm</h4>
         <h1>Soe Lin Aung</h1>
-        <h3> 
-        <TypeAnimation
-          className="typeanimation"
-          repeat={Infinity}
-          sequence={['Web Developer', 1000, 'Web Designer', 1000, 'Fullstack Developer',1000]}
-          wrapper="h3"
-        />
+        <h3>
+          <TypeAnimation
+            className="typeanimation"
+            repeat={Infinity}
+            sequence={[
+              "Web Developer",
+              1000,
+              "Web Designer",
+              1000,
+              "Fullstack Developer",
+              1000,
+            ]}
+            wrapper="h3"
+          />
         </h3>
         <h5 className="text-light">Web Developer</h5>
         <CTA />
-        <HeaderSocials />
-        <div className="me">
-          <img src={ME} alt="me" />
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            justifyContent: "space-between",
+            alignItems: "end",
+          }}
+        >
+          <HeaderSocials />
+          <div className="me">
+            <img src={ME} alt="me" />
+          </div>
+          <div className="scroll__down">
+            <a href="#contact">Scroll Down</a>
+          </div>
         </div>
-
-        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import {BsFacebook, BsInstagram} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
+import React from "react";
+import { BsFacebook, BsInstagram } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
 
 const HeaderSocials = () => {
   return (
-    <div className="header__socials">
-      <a href="https://github.com/soe-cpu" target="_blank"><FaGithub /></a>
-        <a href="https://www.facebook.com/soelinaung709" target="_blank"><BsFacebook /></a>
-        <a href="https://www.instagram.com/sox8206/" target="_blank"><BsInstagram /></a>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        alignItems: "center",
+      }}
+      className="header__socials"
+    >
+      <a href="https://github.com/soe-cpu" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://www.facebook.com/soelinaung709" target="_blank">
+        <BsFacebook />
+      </a>
+      <a href="https://www.instagram.com/sox8206/" target="_blank">
+        <BsInstagram />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
